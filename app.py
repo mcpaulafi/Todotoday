f5c9f3f80b75a90c214805a2613b45202de90158
Paula Meuronen <paula.meuronen@helsinki.fi>
2023-10-19 00:17:46 +0300

    Fixing code based on results from Pylint.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-# Application ToDo Today is made by Paula Meuronen 2023 for Helsinki University course TKT20019
-# app.py: start application and Flask 
- 
-from flask import Flask
+"""users.py: user login and registration"""
+# Application ToDo Today is made by Paula Meuronen 2023
+# for Helsinki University course TKT20019
+
 from os import getenv
+from flask import Flask
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
