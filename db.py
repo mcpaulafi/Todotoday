9195d288eabbe7399fe09f5cd187d7e5c4bad491
Paula Meuronen <paula.meuronen@helsinki.fi>
2023-10-19 01:31:12 +0300

    Fixing code based on results from Pylint.

--- db.py
+++ db.py
@@ -1,6 +1,10 @@
-from app import app
-from flask_sqlalchemy import SQLAlchemy
+"""db.py: database connection"""
+# Application ToDo Today is made by Paula Meuronen 2023
+# for Helsinki University course TKT20019
+
 from os import getenv
+from flask_sqlalchemy import SQLAlchemy
+from app import app
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
